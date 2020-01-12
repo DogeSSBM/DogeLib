b1bbd975b020508fad90f596f734a134c4a17537
DogeSSBM <dogessbm@gmail.com>
2020-01-12 18:20:46 -0500

    cleanup

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #define DOGE_GFX
-#define DOGE_CLI
 #include "Includes.h"
 
 int main(int argc, char const *argv[])
 {
-	#ifdef DOGE_GFX
 	init(800, 600);
 	setColor(RED);
 	fillCircle(400, 300, 100);
@@ -13,13 +11,5 @@ int main(int argc, char const *argv[])
 		events();
 		delay(5);
 	}
-	#else
-	init();
-	printf("Enter num1\n");
-	uint n1 = getInt();
-	printf("Enter num2\n");
-	uint n2 = getInt();
-	printf("Num1 + num2 = %u\n", n1 + n2);
-	#endif
 	return 0;
 }
