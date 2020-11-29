ee2239f6a8987d4fca9683605e8cb26dd8c53d74
dogeThinkpad <dogessbm@gmail.com>
2020-11-29 17:04:38 -0500

    Automatic commit.

--- Text.h
+++ Text.h
@@ -153,7 +153,7 @@ void text_init(void)
 	gfx.fontColor = WHITE;
 	setFontSize(32);
 	if(!gfx.font){
-		printf("Unable to open font or set font size!\n", TTF_GetError());
+		printf("Unable to open font or set font size! Error: %s\n", TTF_GetError());
 		exit(0);
 	}
 	printf("Adding text_quit to atexit()\n");
