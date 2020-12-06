22e27c942c3f285ce35621642fbccc90f45f29c1
dogeThinkpad <dogessbm@gmail.com>
2020-12-05 23:08:30 -0500

    Automatic commit.

--- main.c
+++ main.c
@@ -8,25 +8,18 @@ Coord move(Coord pos, const Length window)
 		keyState(SDL_SCANCODE_S) || keyState(SDL_SCANCODE_DOWN),
 		keyState(SDL_SCANCODE_A) || keyState(SDL_SCANCODE_LEFT)
 	};
-	for(uint i = 0; i < 4; i++){
+	for(Direction i = DIR_U; i <= DIR_L; i++)
 		if(dirs[i] && !dirs[dirINV(i)])
-			pos = coordWrap(
-				coordShift(pos,i,4),
-				(Range){0,window.x},
-				(Range){0,window.y}
-			);
-	}
-	return pos;
+				coordShift(pos,i,4)
+	return coordWrap(pos, (Range){0,window.x}, (Range){0,window.y});
 }
 
 void trackMouse(void)
 {
-	const Coord mnxt = coordAdd(mouse.pos, mouse.vec);
-	const Coord mprv = coordSub(mouse.pos, mouse.vec);
 	setColor(BLUE);
-	drawLineCoords(mouse.pos, mnxt);
+	drawLineCoords(mouse.pos, coordAdd(mouse.pos, mouse.vec));
 	setColor(RED);
-	drawLineCoords(mouse.pos, mprv);
+	drawLineCoords(mouse.pos, coordSub(mouse.pos, mouse.vec));
 	setColor(WHITE);
 	drawPixelCoord(mouse.pos);
 }
