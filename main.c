017b3e0795e5a33b7a6cbdef6f556fc9c074c45c
DogeSSBM <dogessbm@gmail.com>
2021-01-21 16:06:13 -0500

    Automatic commit.

--- main.c
+++ main.c
@@ -1,42 +1,59 @@
 #include "Includes.h"
 
+Coord move(Coord pos, const Length window)
+{
+	const bool dirs[4] = {
+		keyState(SDL_SCANCODE_W) || keyState(SDL_SCANCODE_UP),
+		keyState(SDL_SCANCODE_D) || keyState(SDL_SCANCODE_RIGHT),
+		keyState(SDL_SCANCODE_S) || keyState(SDL_SCANCODE_DOWN),
+		keyState(SDL_SCANCODE_A) || keyState(SDL_SCANCODE_LEFT)
+	};
+	for(Direction i = DIR_U; i <= DIR_L; i++)
+		if(dirs[i] && !dirs[dirINV(i)])
+				pos = coordShift(pos,i,4);
+	return coordWrap(pos, (Range){0,window.x}, (Range){0,window.y});
+}
+
+void trackMouse(void)
+{
+	setColor(BLUE);
+	drawLineCoords(mouse.pos, coordOffset(mouse.pos, mouse.vec));
+	setColor(WHITE);
+	drawPixelCoord(mouse.pos);
+}
+
+void wrapText(const Coord pos, const Coord window, const char *text)
+{
+	const Length textBound = {(gfx.fontSize*strlen(text))/2, gfx.fontSize};
+	for(Direction i = DIR_U; i <= DIR_L; i++){
+		if(coordInRangePair(
+		coordShift(pos,i,dirUD(i)?textBound.y:textBound.x),
+		(RangePair){(Range){0,window.x},(Range){0,window.y}}))
+			continue;
+		drawTextCenteredCoord(coordShift(
+			pos,dirINV(i),dirUD(i)?window.y:window.x),text
+		);
+	}
+
+}
+
 int main(int argc, char const *argv[])
 {
 	const Length window = {800, 600};
 	init(window);
 	setFontSize(48);
-
-	for(uint i = 0; i < 100; i++){
-		int n = randRange(-20, 20);
-		int min = randRange(-99, 99);
-		int max = randRange(-99, 99);
-
-		int err = 0;
-		int ans = n;
-		int size = abs(min-max);
-		if(!inBound(n, min, max)){
-			if(n < min){
-				err = abs(min-n);
-				err %= size;
-				ans = max-err;
-			}else{
-				err = abs(n-max);
-				err %= size;
-				ans = min+err;
-			}
-		}else{
-			ans = n;
-		}
-
-		printf("%s wrap(% 2d, % 2d, % 2d) = % 2d\nans = % 2d\n\n",
-			n==ans?"PASSED":"FAILED",n, min, max, wrap(n, min, max), ans);
-
-	}
-	printf("\n");
+	setFontColor(PINK);
+	const char* text = "DogeLib :3";
+	Coord pos = coordDiv(window, 2);
 
 	while(1){
 		Ticks frameStart = getTicks();
 		clear();
+
+		pos = move(pos, window);
+		trackMouse();
+		drawTextCenteredCoord(pos, text);
+		wrapText(pos, window, text);
 
 		draw();
 		events(frameStart + TPF);
