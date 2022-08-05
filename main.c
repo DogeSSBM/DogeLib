1f49849020c763f27a35ce83564e4d131b5cfd92
DogeSSBM <dogessbm@gmail.com>
2022-08-04 22:35:24 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -6,15 +6,7 @@ int main(int argc, char const *argv[])
     Length window = {.x = 800, .y = 600};
     init();
     setWindowLen(window);
-
-    Coord pos = coordDiv(getWindowLen(), 2);
     Texture *doggo = loadTexture("./Doggo16x16.png");
-    const Scancode dirKey[4] = {
-        SDL_SCANCODE_UP,
-        SDL_SCANCODE_RIGHT,
-        SDL_SCANCODE_DOWN,
-        SDL_SCANCODE_LEFT
-    };
 
     while(1){
        const uint t = frameStart();
