f9e2d55e8141bbfe823763496a508d84ace5590e
DogeSSBM <dogessbm@gmail.com>
2022-09-08 20:08:35 -0400

    added file and func to panic

--- DogeLib/Panic.h
+++ DogeLib/Panic.h
@@ -1,16 +1,21 @@
 #ifndef PANIC_H
 #define PANIC_H
 
-void panicHelper(const int line, const char * format, ... )
+void panic_helper(const char *func, const char *file, const int line, const char * format, ...)
 {
-    fprintf(stderr, "Line %i: ", line);
+    fprintf(stderr, "Panic -\n");
+    fprintf(stderr, "\tFile: %s\n", file);
+    fprintf(stderr, "\tLine: %i\n", line);
+    fprintf(stderr, "\tFunc: %s\n", func);
+    fprintf(stderr, "\tMessage: \"");
     va_list args;
-    va_start (args, format);
-    vprintf (format, args);
-    va_end (args);
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fprintf(stderr, "\"\n");
     exit(EXIT_FAILURE);
 }
 
-#define panic(...)      panicHelper(__LINE__, __VA_ARGS__)
+#define panic(...)  panic_helper(__func__, __FILE__, __LINE__, __VA_ARGS__)
 
 #endif /* end of include guard: PANIC_H */
